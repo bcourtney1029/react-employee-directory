6f43c223672dff762e33bac8b87ab8db64ad0eac
Bryan Courtney <59741945+bcourtney1029@users.noreply.github.com>
2020-10-24 17:07:18 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-function Nav({setSearchTerm, searchTerm, sortByName}) {
+function Nav({handleSearchTerm, searchTerm, sortByName}) {
     return (
         <div className="md:flex justify-between my-5">
             <div className="md:mb-0 mb-4">
-                <button onClick={sortByName} className="bg-primary focus:bg-white text-gray-600">Filter by Name</button>
+                <button onClick={sortByName} className="bg-primary focus:bg-white text-gray">Ascending/Descending Names</button>
             </div>
             <form>
                 <input
                     value={searchTerm}
-                    onChange={setSearchTerm}
+                    onChange={handleSearchTerm}
                     className="w-full py-2 px-2"
                     type="text"
                     placeholder="search for an employee"/>
